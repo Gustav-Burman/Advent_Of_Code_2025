3020c64458d6c0a75c173a0a59c9e50711443c4e
Gustav <gustavburman27@gmail.com>
2026-01-23 15:10:59 +0100

    Moved splitString to header file

--- 02/main.cpp
+++ 02/main.cpp
@@ -1,24 +1,21 @@
 #include "../lib/lib.h"
 #include <iostream>
 #include <vector>
-#include <sstream>
 #include <numeric>
 
 // Local function declarations
-template <typename T> std::vector<T> splitString(const std::string& str, const char delimiter);
-
 std::vector<long int> findRepeatedDigits(std::vector<long int> range);
 
 int main()
 {
-    std::vector<std::string> rawData {Library::readLines("02/input.txt")};
-    const std::vector<std::string> rangesStr {splitString<std::string>(rawData[0], ',')};
+    std::vector<std::string> rawData {Library::readLines("02/test_input.txt")};
+    const std::vector<std::string> rangesStr {Library::splitString<std::string>(rawData[0], ',')};
     
     std::vector<long int> repeatedDigits;
     for (const auto& interval : rangesStr)
     {
         // std::cout << "interval: " << interval << "\n";
-        std::vector<long int> range {splitString<long int>(interval, '-')};
+        std::vector<long int> range {Library::splitString<long int>(interval, '-')};
         std::vector<long int> temp {findRepeatedDigits(range)};
         repeatedDigits.insert(repeatedDigits.end(), temp.begin(), temp.end());
     }
@@ -26,48 +23,10 @@ int main()
     auto sum {std::reduce(repeatedDigits.begin(), repeatedDigits.end())};
     std::cout << "Sum of IDs: " << sum << "\n";
     
-
-    // for each range: find the repeated digits
-    // sum the repeated digits
     return 0;
 }
 
 // Local function definitions
-template <typename T> std::vector<T> splitString(const std::string& str, const char delimiter)
-{
-    std::vector<T> splits;
-    int startIndex {0};
-    int endIndex {0};
-    int size;
-
-    while (endIndex != str.npos)
-    {
-        endIndex = str.find(delimiter, startIndex);
-        size = endIndex - startIndex;
-        // std::cout << str.substr(startIndex, size) << "\n";
-        // If type template is string
-        if constexpr (std::is_same_v<T, std::string>)
-        {
-            splits.push_back(str.substr(startIndex, size));
-            // std::cout << str.substr(startIndex, size) << "\n";
-        }
-        else // If type template is a number
-        {
-            std::stringstream converter(str.substr(startIndex, endIndex));
-            T value;
-            if (converter >> value && converter.eof())
-            {
-                splits.push_back(value);
-                // std::cout << "digit: " << value << "\n";
-            }
-        }
-        startIndex = endIndex + 1;
-        // std::cout << "start: " << startIndex << " end: " << endIndex << "\n";
-    }
-
-    return splits;
-}
-
 bool isRepeatedNumber(long int x)
 {
     std::string numberString = std::to_string(x);
--- lib/lib.h
+++ lib/lib.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <sstream>
 
 namespace Library
 {
@@ -11,6 +12,48 @@ namespace Library
     Reads a textfile and puts every line in a vector<string>
     */
     std::vector<std::string> readLines(std::string path);
+
+    /*
+    ** Function: splitString
+    Splits a string input based on a delimiter.
+
+    return: A vector with all the subtokens from the string 
+    */
+    template <typename T> std::vector<T> splitString(const std::string& str, const char delimiter);
+
+
+    // Function definitions
+    template <typename T> std::vector<T> splitString(const std::string& str, const char delimiter)
+    {
+        std::vector<T> splits;
+        int startIndex {0};
+        int endIndex {0};
+        int size;
+
+        while (endIndex != str.npos)
+        {
+            endIndex = str.find(delimiter, startIndex);
+            size = endIndex - startIndex;
+
+            // If type template is string
+            if constexpr (std::is_same_v<T, std::string>)
+            {
+                splits.push_back(str.substr(startIndex, size));
+            }
+            else // If type template is a number
+            {
+                std::stringstream converter(str.substr(startIndex, endIndex));
+                T value;
+                if (converter >> value && converter.eof())
+                {
+                    splits.push_back(value);
+                }
+            }
+            startIndex = endIndex + 1;
+        }
+
+        return splits;
+    }
 }
 
 #endif
